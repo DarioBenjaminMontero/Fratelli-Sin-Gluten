d351a90d54fd84d2c5bcc72dcd4667d70a66e940
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2026-06-23 20:47:55 -0300

    Agrego imagenes de fratelli e instagram, hago el footer y header

---
+++ views/head.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Fratelli sin gluten</title>
+        <!-- conexiones -->
+        <link rel="stylesheet" href="../CSS/estilosPaginaPrincipal.css">
+      
+        <link rel="icon" type="image/png" href="../imagenes/logo-fratelli.png">
