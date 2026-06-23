9559c73c1aa665eb4d74209a7f43b27bf3541007
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-06-23 20:59:46 -0300

    subo weas

---
+++ views/main.php
@@ -0,0 +1,14 @@
+<div class="burbujaCategorias">
+    <div class="categoria" data-categoria="alfajores"></div>
+    <div class="categoria" data-categoria="panaderia"></div>
+    <div class="categoria" data-categoria="galletas"></div>
+    <div class="categoria" data-categoria="budines"></div>
+    <div class="categoria" data-categoria="postres"></div>
+    <div class="categoria" data-categoria="comidas"></div>
+    <div class="categoria" data-categoria="promociones"></div>
+    <div class="categoria" data-categoria="jugos"></div>
+</div>
+
+<div id="recomendaciones"></div>
+
+<div id="masVendidos"></div>
