e02caa0156e27d7f6e888a303941c0c2e9ba4333
emiliano-estra <147885831+emiliano-estra@users.noreply.github.com>
2026-06-23 22:59:14 -0300

    Add files via upload

---
+++ Fratelli-Sin-Gluten/views/Login.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="../CSS/estiloLogin.css" />
+  <div class="login-container">
+    <img src="../imagenes/logo-fratelli.png" alt="Logo" class="logo" />
+    <h2>Iniciar Sesión</h2>
+    <form action="#" method="POST">
+      <div class="form-group">
+        <label for="Usuario">Nombre De Usuario</label>
+        <input type="USUARIO" id="emLogin" name="USER"  required />
+      </div>
+      <div class="form-group">
+        <label for="password">Contraseña</label>
+        <input type="password" id="passwordLogin" name="password" required />
+      </div>
+      <button type="button" id="boton-Login">Entrar</button>
+    </form>
+    <br>
+    <label id="errores"></label>
+    <div class="register-link">
+      ¿No tienes cuenta? <a href="index.php?page=Registro">Regístrate</a>
+    </div>
+  </div>
+  <script src="../JS/login.js"></script>
---
+++ Fratelli-Sin-Gluten/views/footer.php
@@ -0,0 +1,11 @@
+ <link rel="stylesheet" href="../CSS/estilosFooter.css">
+
+
+<div id="barraFinal">
+    <p><a href="../views/index.php?page=creadores">Sobre nosotros</a></p>
+</div>
+<div id="redes sociales">
+    <a href="https://www.instagram.com/fratelli_singluten/" target="_blank">
+        <img src="../imagenes/instagram.png" alt="Instagram" class="redes">
+    </a>
+</div>
