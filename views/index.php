c64d70976e3948b9c16d4066da2c16857ff77a4b
DarioBenjaminMontero <dariomontero2008@gmail.com>
2026-06-28 16:49:35 -0300

    cambios

--- views/index.php
+++ views/index.php
@@ -9,7 +9,15 @@ case 'main':
     $vistaHeader = './header.php';
     $head = './head.php';
     $footer = './footer.php';
+    $css = '../CSS/cssMain.css';
 break;
+case 'Login':
+    $vista = './Login.php';
+    $vistaHeader = './header.php';
+    $head = './head.php';
+    $footer = './footer.php';
+    $css = '../CSS/cssLogin.css';
+    break;
 }
 include(VIEWS_PATH . 'layout.php');
 ?>
