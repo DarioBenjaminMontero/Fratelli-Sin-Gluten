f0098b3c4e31472ac3405f345dc60db1e2f7b65f
emiliano-estra <147885831+emiliano-estra@users.noreply.github.com>
2026-08-05 14:48:43 -0300

    Delete Fratelli-Sin-Gluten directory

--- Fratelli-Sin-Gluten/views/head.php
+++
@@ -1,9 +0,0 @@
-<meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Fratelli sin gluten</title>
-
-        <link rel="stylesheet" href="<?= $css ?>">
-        <link rel="stylesheet" href="../CSS/cssHeader.css">
-        <link rel="stylesheet" href="../CSS/cssFooter.css">
-      
-        <link rel="icon" type="image/png" href="../imagenes/fratelli.png">
