2a5f9950ed8284098a29cea3f8436c0749caf2f3
emiliano-estra <147885831+emiliano-estra@users.noreply.github.com>
2026-08-05 14:48:04 -0300

    Add files via upload

---
+++ Fratelli-Sin-Gluten/views/header.php
@@ -0,0 +1,52 @@
+<?php require_once '../config/config.php'; ?>
+
+<header class="header">
+
+    <!-- Logo -->
+    <a href="index.php?page=main" class="logo">
+        <img src="../imagenes/fratelli.png" alt="Logo Fratelli" class="logo-img">
+    </a>
+
+    <!-- Menú de navegación -->
+    <nav class="header-left">
+
+        <button id="botonMenu">☰ Menú</button>
+
+        <ul id="menu">
+            <li id="li-inicio">Inicio</li>
+        </ul>
+
+    </nav>
+
+    <!-- Usuario -->
+    <div class="header-right">
+
+        <?php if(isset($_SESSION['user_id'])): ?>
+
+            <div class="user-profile">
+
+                <span class="saludo">
+                    ¡Hola, <?= htmlspecialchars($_SESSION['nombre_usuario']) ?>!
+                </span>
+
+                <button id="botonPerfil">Perfil</button>
+
+                <ul>
+                    <li id="li-carrito">🛒 Carrito</li>
+                </ul>
+
+            </div>
+
+        <?php else: ?>
+            <button id="botonRegister">Registrarse</button>
+            <button id="botonLogin">Iniciar sesión</button>
+
+        <?php endif; ?>
+
+    </div>
+
+</header>
+
+<script src="../JS/buscarUsuario.js"></script>
+<script src="../JS/botonlogin.js"></script>
+<script src="../JS/botonregistro.js"></script>
